d6d4759cc8d91af44191d0e244a3fea07c9fe31c
Devansh Mishra <devanshunique@gmail.com>
2023-01-14 18:44:08 +0530

    Added Logo and Text for Buttons

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Gmail from "../images/Gmail.png"
+import LinkedIn from "../images/LinkedIn.png"
 
 export default function About() {
     return (
@@ -7,8 +9,14 @@ export default function About() {
             <p className="about--title">Frontend Developer</p>
             <p className="about--website">laurasmith.website</p>
             <div className="about-buttons">
-                <button>Email</button>
-                <button>LinkedIn</button>
+                <button>
+                    <img src={Gmail} alt="Email Address"/>
+                    <p>Email</p>
+                </button>
+                <button>
+                    <img src={LinkedIn} alt="LinkedIn Link"/>
+                    <p>LinkedIn</p>
+                </button>
             </div>
             <p className="about--header">About</p>
             <p className="about--content">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
